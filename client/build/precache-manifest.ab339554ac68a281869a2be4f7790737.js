36b9508ee801f333f0934d4dbc49e6ba2f7992a5
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2020-02-09 16:34:40 +0000

    run build

---
+++ client/build/precache-manifest.ab339554ac68a281869a2be4f7790737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc7a2985099ea9c0b02ab92b4d38cf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f74fbfdd962f8aac2022",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "fca5c34a565aeeba887e",
+    "url": "/static/css/main.0c6141d4.chunk.css"
+  },
+  {
+    "revision": "f74fbfdd962f8aac2022",
+    "url": "/static/js/2.aad158ca.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.aad158ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca5c34a565aeeba887e",
+    "url": "/static/js/main.d5904dd4.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
